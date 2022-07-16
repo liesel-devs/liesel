32a6682ac94514fcca88984741d791b813eeaabb
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-07-16 18:15:10 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = "liesel"
-copyright = "2022, Paul Wiemann, Hannes Riebl"
-author = "Paul Wiemann, Hannes Riebl"
+copyright = "2022, Hannes Riebl & Paul Wiemann"
+author = "Hannes Riebl, Paul Wiemann"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,7 +27,11 @@ author = "Paul Wiemann, Hannes Riebl"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.napoleon", # parse numpy and google style docstrings
+    "sphinx.ext.autosummary", # for automatic API doc tables
+    "sphinx.ext.linkcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -39,6 +43,7 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
+pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -55,3 +60,13 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+    filename = info["module"].replace(".", "/")
+    
+    return f"https://github.com/liesel-devs/liesel/blob/main/{filename}.py"
