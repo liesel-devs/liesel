4e5a2cf836a3af4a60715ee332098a7cdf8c42df
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-09-15 11:20:06 +0200

    Create docs.py

---
+++ liesel/docs.py
@@ -0,0 +1,36 @@
+from functools import wraps
+
+
+def usedocs(of: object):
+    """
+    Replaces the docstring of the decorated object with the docstring of the specified
+    object.
+
+    In classes, methods need to be decorated individually. Attribute docstrings are not
+    covered by this decorator.
+
+    Usage example::
+
+        class ParentClass:
+            '''Parentclass documentation'''
+
+            def parent_method(self):
+                '''Method documentation.'''
+
+        @usedocs(ParentClass)
+        class InheritingClass:
+
+            @usedocs(ParentClass.parent_method)
+            def parent_method(self):
+                pass
+    """
+
+    def _use_docs(obj):
+        @wraps(obj)
+        def wrapper():
+            obj.__doc__ = of.__doc__
+            return obj
+
+        return wrapper()
+
+    return _use_docs
