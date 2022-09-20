e1f6e9a0b88701fdd00b6e5ed46060cdf31594ef
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2022-09-20 10:21:02 +0200

    Update docs.py

--- liesel/docs.py
+++ liesel/docs.py
@@ -7,7 +7,7 @@ def usedocs(of: object, greedy: bool = True):
     Replaces the docstring of the decorated object with the docstring of the specified
     object.
 
-    Some notes on the details:
+    Works on both functions and classes. Some notes on the details:
 
     1. By default, this decorator will paste the class docstring from the object
        ``of`` to the decorated object.
@@ -16,7 +16,7 @@ def usedocs(of: object, greedy: bool = True):
        decorated object. This only works for methods and properties that have the same
        name in both objects. This behavior can be turned off by setting the
        argument ``greedy`` to ``False``.
-    3. It is also possible to decorated individual methods and properties to reuse
+    3. It is also possible to decorate individual methods and properties to reuse
        docstrings from specific objects.
     4. Existing docstrings will never be overwritten.
 
