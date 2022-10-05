07077597d7d0dd313eac5bb34f51767187cf0e30
Hannes Riebl <hriebl@uni-goettingen.de>
2022-10-05 18:29:16 +0200

    Minor docstring improvements

--- liesel/docs.py
+++ liesel/docs.py
@@ -1,21 +1,36 @@
+"""
+Docstring utilities.
+"""
+
 from functools import wraps
 from inspect import getmembers, isfunction
 
 
+def _is_prop_or_method(x: object) -> bool:
+    """
+    Helper for selecting methods and properties of classes with
+    :func:`inspect.getmembers`.
+
+    Does not use :func:`inspect.ismethod`, because that returns *True* only for methods
+    of class instances but not for the methods of the classes themselves.
+    """
+    return isfunction(x) or isinstance(x, property)
+
+
 def usedocs(of: object, greedy: bool = True):
     """
-    Replaces the docstring of the decorated object with the docstring of the specified
-    object.
+    Fills missing docstrings of the decorated object with the docstrings from the
+    specified object.
 
     Works on both functions and classes. Some notes on the details:
 
-    1. By default, this decorator will paste the class docstring from the object
-       ``of`` to the decorated object.
-    2. If the decorated object is a class, the decorator will paste the docstrings of
-       methods and properties on the object ``of`` to their counterparts on the
+    1. By default, the decorator will paste the docstring from the object ``of`` to the
+       decorated object.
+    2. If the decorated object is a class, the decorator will also paste the docstrings
+       of the methods and properties of the object ``of`` to their counterparts of the
        decorated object. This only works for methods and properties that have the same
-       name in both objects. This behavior can be turned off by setting the
-       argument ``greedy`` to ``False``.
+       name for both objects. This behavior can be turned off by setting the argument
+       ``greedy`` to ``False``.
     3. It is also possible to decorate individual methods and properties to reuse
        docstrings from specific objects.
     4. Existing docstrings will never be overwritten.
@@ -46,8 +61,9 @@ def usedocs(of: object, greedy: bool = True):
             if not greedy:
                 return obj
 
-            methods_list = getmembers(obj, predicate=_is_prop_or_method)
-            for name, method in methods_list:
+            method_list = getmembers(obj, predicate=_is_prop_or_method)
+
+            for name, method in method_list:
                 if method.__doc__:
                     continue
 
@@ -63,14 +79,3 @@ def usedocs(of: object, greedy: bool = True):
         return wrapper()
 
     return _usedocs
-
-
-def _is_prop_or_method(x: object) -> bool:
-    """
-    Helper for selecting methods and properties of classes with
-    :func:`inspect.getmembers`.
-
-    Does not use :func:`inspect.ismethod`, because that is *True* only for methods on
-    class instances, not on classes themselves.
-    """
-    return isfunction(x) or isinstance(x, property)
