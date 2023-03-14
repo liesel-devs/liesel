3424f1f8a7930b268c95fd65fd636fa15754035f
wiep <wiep@users.noreply.github.com>
2023-03-14 16:34:52 -0500

    Extend documentation
    The documentation includes an example on how to interpret the parts of inference data as the log-likelihood.

--- liesel/experimental/arviz.py
+++ liesel/experimental/arviz.py
@@ -20,6 +20,37 @@ def to_arviz_inference_data(
         The sampling results.
     include_warmup
         Whether to include the warmup in the returned object.
+
+
+    Returns
+    -------
+    The inference data.
+
+
+    Notes
+    -----
+    The inference data has a variable for each position key included in the
+    SamplingResult object. These are usually the position keys of the sampled
+    parameters. Goose can track more values if specified in the field
+    ``position_included``. This might be helpful to let arviz calculate
+    information criteria like WAIC. Assuming that the position key
+    ``loglik_pointwise`` correspond to the point-wise evaluated log-likelihood
+    then the inference data can be slightly changed so that arviz interprets
+    these values as intended.
+
+    .. code-block:: python
+
+        # re-interpret the data
+        llpw_extracted = idat.posterior['loglik_pointwise']
+        idat.posterior = idat.posterior.drop('loglik_pointwise')
+        idat.add_groups({'log_likelihood': {'observed': llpw_extracted}})
+        az.waic(idat)
+
+        # Computed from 300 posterior samples and 10 observations log-likelihood matrix.
+        #              Estimate     SE
+        # elpd_waic.     -17.43   3.40
+        # p_waic           0.16      -
+
     """
 
     posterior_samples = results.get_posterior_samples()
