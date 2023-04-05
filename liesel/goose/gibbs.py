88e2245e12cb552c909498af6ceb822878a01473
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2023-04-05 15:14:10 +0200

    add categorical gibbs kernel function

--- liesel/goose/gibbs.py
+++ liesel/goose/gibbs.py
@@ -1,9 +1,11 @@
 """
 Gibbs sampler.
 """
-
 from collections.abc import Callable, Sequence
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar
+
+import jax
+import jax.numpy as jnp
 
 from .epoch import EpochState
 from .kernel import (
@@ -14,7 +16,18 @@ from .kernel import (
     TuningOutcome,
     WarmupOutcome,
 )
-from .types import Kernel, KernelState, KeyArray, ModelState, Position, TuningInfo
+from .types import (
+    Array,
+    Kernel,
+    KernelState,
+    KeyArray,
+    ModelState,
+    Position,
+    TuningInfo,
+)
+
+if TYPE_CHECKING:
+    from ..model.model import Model
 
 GibbsKernelState = KernelState
 GibbsTransitionInfo = DefaultTransitionInfo
@@ -123,3 +136,85 @@ class GibbsKernel(
         """
 
         return WarmupOutcome(error_code=0, kernel_state=kernel_state)
+
+
+def create_categorical_gibbs_kernel(
+    name: str, outcomes: Sequence[Array], model: "Model"
+) -> GibbsKernel:
+    """
+    Creates a categorical Gibbs kernel.
+
+    The prior distribution of the variable to sample must be a categorical distribution,
+    usually implemented via :class:`tfd.FiniteDiscrete`.
+
+    This kernel evaluates the full conditional log probability of the model for each
+    possible value of the variable to sample. It then draws a new value for the variable
+    from the categorical distribution defined by the full conditional log probabilities.
+
+    Usually, you can define more efficient specialized kernels if you know the actual
+    full conditional distribution of the variable to sample.
+
+    Parameters
+    ----------
+    name
+        The name of the variable to sample.
+    outcomes
+        List of possible outcomes.
+    model
+        The model to sample from.
+
+    Examples
+    --------
+
+    In the following example, we create a categorical Gibbs kernel for a variable
+    with three possible values. The prior distribution of the variable is a categorical
+    distribution with probabilities ``[0.1, 0.2, 0.7]``.
+
+    You can then use the kernel to sample from the model.
+
+    >>> import tensorflow_probability.substrates.jax.distributions as tfd
+
+    >>> values = [0, 1, 2]
+    >>> prior_probs = [0.1, 0.2, 0.7]
+    >>> value_grid = lsl.Var(values, name="value_grid")
+
+    >>> prior = lsl.Dist(tfd.FiniteDiscrete, outcomes=value_grid, probs=prior_probs)
+    >>> categorical_var = lsl.Var(
+    ...     value=values[0],
+    ...     distribution=prior,
+    ...     name="categorical_var",
+    ... )
+
+    >>> model = lsl.GraphBuilder().add(categorical_var).build_model()
+    >>> kernel = create_categorical_gibbs_kernel("categorical_var", values, model)
+    >>> type(kernel)
+    <class 'liesel.goose.gibbs.GibbsKernel'>
+
+    """
+
+    model = model._copy_computational_model()
+
+    def transition_fn(prng_key, model_state):
+
+        model.state = model_state
+        for node in model.nodes.values():
+            node._outdated = False
+
+        def conditional_log_prob_fn(value: Array):
+            """
+            Evaluates the full conditional log probability of the model
+            given the input value.
+            """
+            model.vars[name].value = value
+            return model.log_prob
+
+        conditional_log_probs = jax.tree_map(conditional_log_prob_fn, outcomes)
+
+        draw_index = jax.random.categorical(
+            prng_key, logits=jnp.stack(conditional_log_probs)
+        )
+        draw = jnp.array(outcomes)[draw_index]
+
+        return {name: draw}
+
+    return GibbsKernel([name], transition_fn)
--- tests/goose/kernels/test_gibbs.py
+++ tests/goose/kernels/test_gibbs.py
@@ -2,10 +2,17 @@ import jax.numpy as jnp
 import jax.random
 import numpy as np
 import pytest
+import tensorflow_probability.substrates.jax.distributions as tfd
 from model_lm import beta_ols, model_state, run_kernel_test
 
 import liesel.goose as gs
-from liesel.goose.gibbs import GibbsKernelState, GibbsTransitionInfo, GibbsTuningInfo
+import liesel.model as lsl
+from liesel.goose.gibbs import (
+    GibbsKernelState,
+    GibbsTransitionInfo,
+    GibbsTuningInfo,
+    create_categorical_gibbs_kernel,
+)
 from liesel.goose.types import Kernel
 
 XX_inv = np.linalg.inv(model_state["X"].T @ model_state["X"])
@@ -28,3 +35,80 @@ def test_gibbs(mcmc_seed):
     kernel1 = gs.NUTSKernel(["log_sigma"])
 
     run_kernel_test(mcmc_seed, [kernel0, kernel1])
+
+
+class TestGeneralCategoricalGibbsKernel:
+    def test_transition(self):
+        values = [0, 1, 2]
+        prior_probs = [0.1, 0.2, 0.7]
+        value_grid = lsl.Var(values, name="value_grid")
+
+        prior = lsl.Dist(tfd.FiniteDiscrete, outcomes=value_grid, probs=prior_probs)
+        categorical_var = lsl.Var(
+            value=values[0],
+            distribution=prior,
+            name="categorical_var",
+        )
+
+        model = lsl.GraphBuilder().add(categorical_var).build_model()
+        kernel = create_categorical_gibbs_kernel("categorical_var", values, model)
+
+        draw = kernel._transition_fn(jax.random.PRNGKey(0), model.state)
+        assert draw["categorical_var"] == pytest.approx(0)
+
+        draw = kernel._transition_fn(jax.random.PRNGKey(1), model.state)
+        assert draw["categorical_var"] == pytest.approx(2)
+
+        draw = kernel._transition_fn(jax.random.PRNGKey(2), model.state)
+        assert draw["categorical_var"] == pytest.approx(1)
+
+    def test_transition_jit(self):
+        values = [0, 1, 2]
+        prior_probs = [0.1, 0.2, 0.7]
+        value_grid = lsl.Var(values, name="value_grid")
+
+        prior = lsl.Dist(tfd.FiniteDiscrete, outcomes=value_grid, probs=prior_probs)
+        categorical_var = lsl.Var(
+            value=values[0],
+            distribution=prior,
+            name="categorical_var",
+        )
+
+        model = lsl.GraphBuilder().add(categorical_var).build_model()
+        kernel = create_categorical_gibbs_kernel("categorical_var", values, model)
+
+        draw = jax.jit(kernel._transition_fn)(jax.random.PRNGKey(1), model.state)
+        assert draw["categorical_var"] == pytest.approx(2)
+
+    @pytest.mark.mcmc
+    def test_sample(self):
+        values = [0, 1, 2]
+        prior_probs = [0.1, 0.2, 0.7]
+        value_grid = lsl.Var(values, name="value_grid")
+
+        prior = lsl.Dist(tfd.FiniteDiscrete, outcomes=value_grid, probs=prior_probs)
+        categorical_var = lsl.Var(
+            value=values[0],
+            distribution=prior,
+            name="categorical_var",
+        )
+
+        model = lsl.GraphBuilder().add(categorical_var).build_model()
+        kernel = create_categorical_gibbs_kernel("categorical_var", values, model)
+
+        eb = gs.EngineBuilder(1, num_chains=1)
+        eb.add_kernel(kernel)
+        eb.set_model(lsl.GooseModel(model))
+        eb.set_initial_values(model.state)
+        eb.set_duration(warmup_duration=500, posterior_duration=2000)
+
+        engine = eb.build()
+        engine.sample_all_epochs()
+
+        results = engine.get_results()
+        samples = results.get_posterior_samples()
+
+        values, counts = np.unique(samples["categorical_var"], return_counts=True)
+        relative_freq = counts / np.sum(counts)
+
+        assert np.allclose(relative_freq, prior_probs, atol=0.1)
