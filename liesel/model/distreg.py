d8262cb8fba3295a70fcbd3eb0e285ea5497abce
Gianmarco Callegher <gianmarco.callegher@uni-goettinge.de>
2023-11-08 15:12:26 +0100

    Updated tutorial and jitter fn for tau2

--- liesel/model/distreg.py
+++ liesel/model/distreg.py
@@ -31,8 +31,8 @@ from .nodes import Array, Bijector, Dist, Distribution, Group, NodeState, Var
 
 matrix_rank = np.linalg.matrix_rank
 
-tau2_jitter_fn: JitterFunction = lambda key, val: jax.random.uniform(
-    key, val.shape, val.dtype, 1.0, 1000.0
+tau2_jitter_fn: JitterFunction = lambda key, val: jax.random.truncated_normal(
+    key, 0.0, 1e2, val.shape, val.dtype, 1.0
 )
 beta_jitter_fn: JitterFunction = lambda key, val: val + jax.random.uniform(
     key, val.shape, val.dtype, -2.0, 2.0
