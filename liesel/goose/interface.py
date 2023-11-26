0655743910bcf555fe9845c17d1bc6859c1a115a
wiep <wiep@users.noreply.github.com>
2023-11-25 20:34:28 -0600

    Add documentation

--- liesel/goose/interface.py
+++ liesel/goose/interface.py
@@ -340,15 +340,113 @@ class LieselInterface:
 
 
 class NamedTupleInterface:
+    """
+    A model interface for a model state represented by a :obj:`~typing.NamedTuple`
+    and a corresponding log-probability function.
+
+    Parameters
+    ----------
+    log_prob_fn
+        A function that takes a model state and returns the log-probability. The
+        model state is expected to be a :obj:`~typing.NamedTuple`.
+
+    See Also
+    --------
+    .DictInterface : A model interface for a model state represented by a
+        ``dict[str, Array]`` and a corresponding log-probability function.
+    .DataclassInterface : A model interface for a model state represented by a
+        :obj:`~dataclasses.dataclass` and a corresponding log-probability function.
+    .LieselInterface : A model interface for a Liesel :class:`.Model`.
+
+    Examples
+    --------
+
+    For this example, we import ``tensorflow_probability`` as follows:
+
+    >>> import tensorflow_probability.substrates.jax.distributions as tfd
+
+    We define a subclass of NamedTuple representing the model state:
+
+    >>> from typing import NamedTuple
+    ...
+    >>> class State(NamedTuple):
+    ...     x: jnp.ndarray
+    ...     loc: jnp.ndarray
+    ...     scale: jnp.ndarray
+
+    Now we define a very simple log_prob_fn for the sake of demonstration:
+
+    >>> def log_prob_fn(model_state):
+    ...     loc = model_state.loc
+    ...     scale = model_state.scale
+    ...     x = model_state.x
+    ...     return tfd.Normal(loc, scale).log_prob(x)
+
+    We initialize the interface by passing the log_prob_fn to the constructor:
+
+    >>> interface = gs.NamedTupleInterface(log_prob_fn)
+
+    We evaluate the log-probability of a model state by calling the log_prob method:
+
+    >>> state = State(x=jnp.array(0.0), loc=jnp.array(0.0), scale=jnp.array(1.0))
+    >>> interface.log_prob(state)
+    Array(-0.9189385, dtype=float32)
+
+    We update the model state by passing a position to the update_state method:
+
+    >>> position = {"x": jnp.array(1.0)}
+    >>> state = interface.update_state(position, state)
+
+    We can now evaluate the log-probability of the updated model state:
+
+    >>> interface.log_prob(state)
+    Array(-1.4189385, dtype=float32)
+    """
+
     def __init__(self, log_prob_fn: LogProbFunction):
         self._log_prob_fn = log_prob_fn
 
     def extract_position(self, position_keys: Sequence[str], model_state: ModelState):
+        """
+        Extracts a position from a model state.
+
+        Parameters
+        ----------
+        position_keys
+            An iterable of variable or node names.
+        model_state
+            A dictionary of node names and their corresponding :class:`.NodeState`.
+        """
         return {key: getattr(model_state, key) for key in position_keys}
 
     def log_prob(self, model_state: ModelState) -> float:
+        """
+        Updates and returns a model state given a position.
+
+        Parameters
+        ----------
+        position
+            A dictionary of variable or node names and values.
+        model_state
+            A dictionary of node names and their corresponding :class:`.NodeState`.
+
+        Warnings
+        --------
+        The ``model_state`` must be up-to-date, i.e. it must *not* contain any outdated
+        nodes. Updates can only be triggered through new variable or node values in the
+        ``position``. If you supply a ``model_state`` with outdated nodes, these nodes
+        and their outputs will not be updated.
+        """
         return self._log_prob_fn(model_state)
 
     def update_state(self, position, model_state: ModelState) -> ModelState:
+        """
+        Returns the log-probability from a model state.
+
+        Parameters
+        ----------
+        model_state
+            A dictionary of node names and their corresponding :class:`.NodeState`.
+        """
         new_state = model_state._replace(**position)
         return new_state
