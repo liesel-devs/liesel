eba954a81dbc920844d5b466a863614cb3ef2a72
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-05-01 17:28:56 +0200

    Add Model.sample and Var.sample (#248)
    * add parental_submodel method
    
    * add extract_position method
    
    * add update_state method
    
    * add Model.predict method
    
    * add Var.predict method
    
    * add parameters and observed attributes
    
    * implement sample_prior
    
    * introduce TemporaryModel context manager
    
    * implement Model.sample()
    
    * update and test Model.sample()
    
    * Implement Var.sample()
    
    * remove Model.sample_prior()
    
    * fix Model.sample()
    
    * allow replacement distributions
    
    * allow newdata in sample methods
    
    * tidy up test
    
    * re-raise original error class
    
    * add description to TemporaryModel
    
    * improve code organization, add comments
    
    * Add x-failing test for jitted sample function
    
    * formatting
    
    * update key array typing
    
    * move state restoration to finally block
    
    * pre-commit
    
    * enables jit compilation of model.sample
    
    * avoids tracer leaks in one_draw compilations
    
    * remove unnecessary state cleanup
    
    * fix typo
    
    * update jitting test
    
    * update docs
    
    ---------
    
    Co-authored-by: wiep <wiep@users.noreply.github.com>

--- src/liesel/model/nodes.py
+++ src/liesel/model/nodes.py
@@ -8,7 +8,7 @@ import logging
 import warnings
 import weakref
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Hashable, Iterable
+from collections.abc import Callable, Hashable, Iterable, Sequence
 from functools import wraps
 from itertools import chain
 from types import MappingProxyType
@@ -22,6 +22,7 @@ from typing import (
     TypeVar,
 )
 
+import jax
 import tensorflow_probability.substrates.jax.bijectors as jb
 import tensorflow_probability.substrates.jax.distributions as jd
 import tensorflow_probability.substrates.numpy.bijectors as nb
@@ -2030,60 +2031,18 @@ class Var:
                     subgraph = self.model.node_parental_subgraph(*filtered_nodes)
                     return plot_nodes(subgraph, **kwargs)
 
-        from liesel.model import GraphBuilder
+        from liesel.model.model import TemporaryModel
 
-        gb = GraphBuilder().add(self)
-        nodes, _vars = gb._all_nodes_and_vars()
-
-        automatically_set_names = gb._set_missing_names()
-        var_names = automatically_set_names["vars"]
-        node_names = automatically_set_names["nodes"]
-        if var_names:
-            if verbose:
-                names_ = f"The automatically assigned names are: {var_names}. "
-            else:
-                names_ = ""
-            logger.info(
-                f"Unnamed variables were temporarily named for plotting. {names_}"
-                "The names are reset"
-                " after plotting."
-            )
-        if node_names:
-            if verbose:
-                names_ = f"The automatically assigned names are: {node_names}. "
-            else:
-                names_ = ""
-            logger.info(
-                f"Unnamed nodes were temporarily named for plotting. {names_}"
-                "The names are reset"
-                " after plotting."
-            )
-
-        model = gb.build_model()
-
-        match which:
-            case "vars":
-                subgraph = model.var_parental_subgraph(self)
-                plot_vars(subgraph, **kwargs)
-            case "nodes":
-                self_nodes = [self.value_node, self.dist_node, self.var_value_node]
-                filtered_nodes = [nd for nd in self_nodes if nd is not None]
-                subgraph = model.node_parental_subgraph(*filtered_nodes)
-                plot_nodes(subgraph, **kwargs)
-
-        model.pop_nodes_and_vars()
-
-        vars_dict = {var_.name: var_ for var_ in _vars}
-        nodes_dict = {node.name: node for node in nodes}
-
-        for name in var_names:
-            vars_dict[name].name = ""
-
-        for name in node_names:
-            nodes_dict[name].name = ""
-
-        gb.nodes.clear()
-        gb.vars.clear()
+        with TemporaryModel(self, verbose=verbose) as model:
+            match which:
+                case "vars":
+                    subgraph = model.var_parental_subgraph(self)
+                    plot_vars(subgraph, **kwargs)
+                case "nodes":
+                    self_nodes = [self.value_node, self.dist_node, self.var_value_node]
+                    filtered_nodes = [nd for nd in self_nodes if nd is not None]
+                    subgraph = model.node_parental_subgraph(*filtered_nodes)
+                    plot_nodes(subgraph, **kwargs)
 
     def plot_vars(
         self,
@@ -2167,6 +2126,81 @@ class Var:
         submodel = self.model.parental_submodel(self)  # type: ignore
         pred = submodel.predict(samples=samples, predict=[self.name], newdata=newdata)
         return pred[self.name]
+
+    def sample(
+        self,
+        shape: Sequence[int],
+        seed: jax.Array,
+        posterior_samples: dict[str, Array] | None = None,
+        fixed: Sequence[str] = (),
+        newdata: dict[str, Array] | None = None,
+        dists: dict[str, Dist] | None = None,
+    ) -> dict[str, Array]:
+        """
+        Draws samples from the parental model for this variable.
+
+        Parameters
+        ----------
+        shape
+            Sample shape.
+        seed
+            The seed is split and distributed to the seed nodes of the model. \
+            Must be a jax RNG key array that satisfies \
+            ``jnp.issubdtype(key.dtype, jax.dtypes.prng_key)``. \
+            See :mod:`jax.random` and \
+            https://docs.jax.dev/en/latest/jep/9263-typed-keys.html for more details.
+        posterior_samples
+            Dictionary of samples at which to evaluate predictions. All values of the \
+            dictionary are assumed to have two leading dimensions corresponding to \
+            ``(nchains, niteration)``.
+        fixed
+            The names of the nodes or variables to be excluded from the simulation. \
+            By default, no nodes or variables are skipped.
+        newdata
+            Dictionary of new data at which to produce samples. The keys should \
+            correspond to variable or node names in the model whose values should be \
+            set to the given values before sampling. If ``None`` \
+            (default), the current variable values are used.
+        dists
+            Can be used to provide a dictionary of variable names and :class:`.Dist` \
+            instances to use in sampling. If ``None`` (default), samples are drawn for \
+            each variable using their :attr:`.Var.dist_node`.
+
+        Notes
+        -----
+        When compiling this function with ``jax.jit``, the arguments ``shape``,
+        ``fixed``, and ``dists`` must be static.
+
+        Returns
+        -------
+        A dictionary of variable and node names and their sampled values. Includes
+        only sampled variables.
+        """
+        if self.model:
+            submodel = self.model.parental_submodel(self)
+            drawn_samples = submodel.sample(
+                shape=shape,
+                seed=seed,
+                posterior_samples=posterior_samples,
+                fixed=fixed,
+                newdata=newdata,
+                dists=dists,
+            )
+            return drawn_samples
+
+        from .model import TemporaryModel
+
+        with TemporaryModel(self, silent=True) as model:
+            drawn_samples = model.sample(
+                shape=shape,
+                seed=seed,
+                posterior_samples=posterior_samples,
+                fixed=fixed,
+                newdata=newdata,
+                dists=dists,
+            )
+
+            return drawn_samples
 
     def plot_nodes(
         self,
