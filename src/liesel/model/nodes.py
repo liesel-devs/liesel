edb883dc287f658001eee14393ecf2ac1b7e60a0
wiep <wiep@users.noreply.github.com>
2025-05-01 17:07:01 -0400

    filter warning in doctest

--- src/liesel/model/nodes.py
+++ src/liesel/model/nodes.py
@@ -2559,8 +2559,11 @@ class Group:
     --------
     Add a variable to a group:
 
-    >>> my_var = lsl.Var(0.0, name="long_unique_variable_name")
-    >>> grp = lsl.Group(name="demo_group", short_name=my_var)
+    >>> import warnings
+    >>> with warnings.catch_warnings():
+    ...     warnings.simplefilter("ignore")
+    ...     my_var = lsl.Var(0.0, name="long_unique_variable_name")
+    ...     grp = lsl.Group(name="demo_group", short_name=my_var)
     >>> grp
     Group(name="demo_group")
 
