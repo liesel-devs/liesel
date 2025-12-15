0d46c76b502d1242d2a1cbfb8539237dd3aea458
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-12-15 15:49:03 +0100

    Update test_vi.py

--- tests/experimental/optim/test_vi.py
+++ tests/experimental/optim/test_vi.py
@@ -1,27 +1,26 @@
-import liesel.model as lsl
 import liesel.experimental.optim as opt
+import liesel.model as lsl
 
 
 def test_vdist_float64():
     x = lsl.Var.new_obs(
-        0.,
+        0.0,
         name="x",
     )
     model = lsl.Model([x], to_float32=False)
-    q = opt.VDist(["x"], model).normal(0., 1.)
-    
-    assert not q.to_float32
+    q = opt.VDist(["x"], model).normal(0.0, 1.0)
+
+    assert not q.p.to_float32
 
 
 def test_compositevdist_float64():
     x = lsl.Var.new_obs(
-        0.,
+        0.0,
         name="x",
     )
     model = lsl.Model([x], to_float32=False)
-    q = opt.VDist(["x"], model).normal(0., 1.)
-    
+    q = opt.VDist(["x"], model).normal(0.0, 1.0)
+
     vi_dist = opt.CompositeVDist(q).build()
 
-    assert not vi_dist.to_float32
-
+    assert not vi_dist._to_float32()
