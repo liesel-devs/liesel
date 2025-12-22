820dcdc6e6312729259906a5300cb3eadf24a20c
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-12-22 10:43:38 +0100

    add docstrings to copula functionality

--- src/liesel/distributions/copulas.py
+++ src/liesel/distributions/copulas.py
@@ -29,6 +29,47 @@ class GaussianCopula(tfd.TransformedDistribution):
         batch members are undefined.
     name
         Python ``str``, name prefixed to ``Ops`` created by this class.
+
+    Examples
+    --------
+
+    This is an example for how to use this class with Liesel.
+
+    >>> import jax.numpy as jnp
+    >>> import liesel.model as lsl
+    >>> import tensorflow_probability.substrates.jax.bijectors as tfb
+    >>> import tensorflow_probability.substrates.jax.distributions as tfd
+    >>> from liesel.distributions import GaussianCopula
+
+    >>> correlation = lsl.Var.new_param(0.2, bijector=tfb.Tanh(), name="rho")
+    >>> correlation.bijected_var
+    Var(name="h(rho)")
+
+    >>> dist1 = lsl.Dist(tfd.Normal, loc=0.0, scale=1.0)
+    >>> x1 = lsl.Var.new_obs(jnp.array([-0.5, -0.1, -0.25]), dist1).update()
+    >>> dist2 = lsl.Dist(tfd.Normal, loc=0.0, scale=1.0)
+    >>> x2 = lsl.Var.new_obs(jnp.array([0.5, 0.1, 0.25]), dist2).update()
+
+    >>> x1_pit = lsl.PIT(x1, name="PIT(x1)").update()
+    >>> x2_pit = lsl.PIT(x2, name="PIT(x2)").update()
+    >>> x_dependence = lsl.Var.new_calc(
+    ...     lambda *inputs: jnp.stack(inputs, axis=1),
+    ...     x1_pit,
+    ...     x2_pit,
+    ...     distribution=lsl.Dist(GaussianCopula, dependence=correlation),
+    ...     name="x_dependence",
+    ... ).update()
+
+    >>> (x1.log_prob + x2.log_prob + x_dependence.log_prob).sum().round(1)
+    Array(-5.9, dtype=float32)
+
+    Comparing to an ordinary multivariate normal:
+
+    >>> dist = tfd.MultivariateNormalFullCovariance(
+    ...     loc=jnp.zeros(2), covariance_matrix=jnp.array([[1.0, 0.2], [0.2, 1.0]])
+    ... )
+    >>> dist.log_prob(jnp.stack((x1.value, x2.value), axis=-1)).sum().round(1)
+    Array(-5.9, dtype=float32)
     """
 
     def __init__(
@@ -47,7 +88,7 @@ class GaussianCopula(tfd.TransformedDistribution):
             scale_tril = None
         else:
             if validate_args:
-                assert np.all(dependence >= 0.0)
+                assert np.all(dependence >= -1.0)
                 assert np.all(dependence <= 1.0)
 
             tril11 = np.broadcast_to(1.0, batch_shape)
